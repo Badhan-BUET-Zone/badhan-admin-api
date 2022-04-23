a35343c6ddea846eab48f55c5e8df4a9260e28bd
sumaiyaazad <azadsumaiya00@gmail.com>
2022-04-23 11:56:53 +0600

    modified middleware verifycsrftoken.php

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -10,8 +10,11 @@ class VerifyCsrfToken extends Middleware
      * The URIs that should be excluded from CSRF verification.
      *
      * @var array<int, string>
+     *
      */
+
     protected $except = [
         //
+        '/contributors',
     ];
 }
