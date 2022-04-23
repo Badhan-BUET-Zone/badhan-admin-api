9840ac467529f6a56cdd812734623aca7cbe1d85
sumaiyaazad <azadsumaiya00@gmail.com>
2022-04-23 14:27:54 +0600

    post request validation done

--- app/Http/Controllers/FirebaseController.php
+++ app/Http/Controllers/FirebaseController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
+use Carbon\Carbon;
+
 
 class FirebaseController extends Controller
 {
@@ -20,14 +24,61 @@ class FirebaseController extends Controller
         return response()->json(['status'=>200,'data'=>$reference->getvalue()]);
     }
 
-    public function store(){
-        $this->database->getReference('data/')
-            ->set([
-                'mir' => 'Example Task',
-                'turja'=>'hkhggv',
-            ]);
-        $reference = $this->database->getReference('data');
-        return response()->json(['status'=>200,'data'=>$reference->getvalue()]);
+    public function store(Request $request){
+        $messages = [
+            'in' => 'The :attribute must be one of the following values: :values',
+            'min' => 'The :attribute does not have minimum length :min',
+            'array' => 'The :attribute must be an array'
+        ];
+        $validator = Validator::make($request->all(),[
+            'type' => [
+                'required',
+                Rule::in(['Active Developers', 'Contributors of Badhan', 'Legacy Developers']),
+                ],
+            'name' => [
+                'required',
+                'min:3'
+            ],
+            'calender' => [
+                'required',
+                'min:3'
+            ],
+            'contribution' => [
+                'required',
+                'array',
+                'min:1'
+            ],
+            "contribution.*" => [
+                'min:3',
+            ],
+            'links' => [
+                'required',
+                'array',
+                'min:1'
+            ],
+            "links.*.color" => [
+                'required',
+                'min:3',
+            ],
+            "links.*.icon" => [
+                'required',
+                'min:3',
+            ],
+            "links.*.link" => [
+                'required',
+                'url',
+            ],
+        ],$messages);
+        $id = Carbon::now()->timestamp;
+        if ($validator->fails()) {
+            return response()->json(['status'=>400,'data'=>$validator->errors()]);
+        }
+
+        $id2 = $this->database->getReference('data/')
+            ->push($validator->valid());
+        return response()->json(['status'=>200,'data'=>$validator->valid(),'id'=>$id2]);
+//        $reference = $this->database->getReference('data');
+//        return response()->json(['status'=>200,'data'=>$contributor]);
     }
 
     public function update($id){
@@ -41,9 +92,9 @@ class FirebaseController extends Controller
 
     }
 
-    public function destroy(Request $request, $id){
+    public function destroy(Request $input){
 
-        $this->database->getReference('data/'.$id)->remove();
+        $this->database->getReference('data/'.$input->id)->remove();
         return response()->json(['status'=>200]);
     }
 
